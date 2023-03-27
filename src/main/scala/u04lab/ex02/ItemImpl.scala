8151b8371f6ffaf0408a67b4fca56e73f0e98c52
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-03-27 12:41:28 +0200

    Complete ex02.b

--- src/main/scala/u04lab/ex02/ItemImpl.scala
+++ src/main/scala/u04lab/ex02/ItemImpl.scala
@@ -5,4 +5,5 @@ import u04lab.code.{Item, List}
 case class ItemImpl(override val code: Int,
                     override val name: String,
                     override val tags: List[String]) extends Item {
+
 }
